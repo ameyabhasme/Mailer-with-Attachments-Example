dd06204a248cfc282b3ccf9be57a7776933644cd
ameyabhasme <ameyabhasme5@gmail.com>
2021-12-30 19:56:30 +0530

    Added: Mailer with attachments example

---
+++ src/test/java/com/bridgelabz/SendingEmail_Attachment/SendingEmail_AttachmentTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.SendingEmail_Attachment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SendingEmail_AttachmentTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
